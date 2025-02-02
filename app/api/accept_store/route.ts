833a26005ec87a5d2eab4b3a298ad0b96db862a6
Hasafif <hassan.n.afif@gmail.com>
2025-02-01 20:07:20 -0500

    f4

--- app/api/accept_store/route.ts
+++ app/api/accept_store/route.ts
@@ -5,6 +5,7 @@ import { NextRequest, NextResponse } from "next/server";
 async function handler(req: NextRequest) {
   const { storeId} = await req.json();
 
+  
   console.log(storeId);
   if (!storeId) {
     return NextResponse.json(
