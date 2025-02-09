34546224d17cdbcab0d40b8184412c4109066ea2
Hasafif <hassan.n.afif@gmail.com>
2025-02-09 07:25:44 -0500

    f18

--- prisma/migrations/20250209114718_/migration.sql
+++ prisma/migrations/20250209114718_/migration.sql
@@ -5,20 +5,7 @@
   - You are about to drop the `privilages` table. If the table is not empty, all the data it contains will be lost.
 
 */
--- DropForeignKey
-ALTER TABLE `_usertoprivilages` DROP FOREIGN KEY `_UserToprivilages_A_fkey`;
 
--- DropForeignKey
-ALTER TABLE `_usertoprivilages` DROP FOREIGN KEY `_UserToprivilages_B_fkey`;
-
--- AlterTable
-ALTER TABLE `user` ADD COLUMN `privilages` VARCHAR(191) NULL;
-
--- DropTable
-DROP TABLE `_usertoprivilages`;
-
--- DropTable
-DROP TABLE `privilages`;
 
 -- CreateTable
 CREATE TABLE `issue` (
